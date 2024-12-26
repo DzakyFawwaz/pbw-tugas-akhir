acb03c0d23dc2fc812d94b648ad12c548fb7ee13
DzakyFawwaz <dzkyfwzaryd@gmail.com>
2024-12-26 14:18:13 +0700

    on_reads

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
@@ -15,19 +15,26 @@ class DashboardController extends Controller
 
         $selected_book = $request->input(("selected-book"));
 
-        if (!isset($selected_book)) {
-            $selected_book = Arr::first(config('data.books'), fn($book) => $book['is_reading'] == true)['id'];
+
+        $books = DB::table('books')->select('id', 'title', 'rating', 'cover_url');
+        $on_reads = DB::table('books')
+            ->join('histories', 'books.id', '=', 'histories.book_id')
+            ->where('user_id', Auth::user()->id)
+            ->get();
+
+
+        if (!isset($selected_book) && isset($on_reads) && count($on_reads) > 0) {
+            $selected_book = head($on_reads)[0]->id;
         }
 
+
         return view('dashboard', [
-            'books' => DB::table('books')->select('id', 'title', 'rating', 'cover_url'),
+            'books' => $books,
             'trendings' => Book::query()->where('tags', "Trending")->get(),
             'favorites' => Book::query()->where('tags', "Favorite")->get(),
             'selected_book' => Book::query()
-                ->firstWhere('id', $selected_book)
-            // 'selected_book' => Book::query()
-            //     ->firstWhere('id', $selected_book)
-            // ->select('title', 'rating',)
+                ->firstWhere('id', $selected_book),
+            'on_reads' => $on_reads
         ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,72 +1,72 @@
 <x-layout>
 
-     @isset($selected_book)
-     <aside class="aside aside-1">
-         <div class="selected-book">
-             <p class="selected-book-type">{{ $selected_book->genre ?? '' }}</p>
-             <div class="selected-book-image">
-                 <img class="h-80" src="{{ $selected_book->cover_url ?? '' }}" width="200" height="320" />
-             </div>
-             <div class="selected-book-title">
-                 <h1 class="text-2xl my-5">{{ $selected_book->title ?? '' }}</h1>
-             </div>
-             <p class="selected-book-author mb-2 ">{{ $selected_book->author ?? '' }}</p>
-             <div class="selected-book-description">
-                 <span class="text-gray-300">
-                     {{ $selected_book->synopsis ?? '' }}
-                 </span>
-             </div>
-             <div class="selected-book-rating">
-                 <div class="stars">
-                     <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-6">
-                         <path fill-rule="evenodd"
-                             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                             clip-rule="evenodd" />
-                     </svg>
-                     <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-6">
-                         <path fill-rule="evenodd"
-                             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                             clip-rule="evenodd" />
-                     </svg>
-                     <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-6">
-                         <path fill-rule="evenodd"
-                             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                             clip-rule="evenodd" />
-                     </svg>
-                     <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-6">
-                         <path fill-rule="evenodd"
-                             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                             clip-rule="evenodd" />
-                     </svg>
-                     <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                         class="size-6">
-                         <path fill-rule="evenodd"
-                             d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                             clip-rule="evenodd" />
-                     </svg>
-                 </div>
-                 <p>{{ $selected_book->rating ?? '' }}</p>
-             </div>
+    @isset($selected_book)
+        <aside class="aside aside-1">
+            <div class="selected-book">
+                <p class="selected-book-type">{{ $selected_book->genre ?? '' }}</p>
+                <div class="selected-book-image">
+                    <img class="h-80" src="{{ $selected_book->cover_url ?? '' }}" width="200" height="320" />
+                </div>
+                <div class="selected-book-title">
+                    <h1 class="text-2xl my-5">{{ $selected_book->title ?? '' }}</h1>
+                </div>
+                <p class="selected-book-author mb-2 ">{{ $selected_book->author ?? '' }}</p>
+                <div class="selected-book-description">
+                    <span class="text-gray-300">
+                        {{ $selected_book->synopsis ?? '' }}
+                    </span>
+                </div>
+                <div class="selected-book-rating">
+                    <div class="stars">
+                        <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <p>{{ $selected_book->rating ?? '' }}</p>
+                </div>
 
-             <a href="/book/{{ $selected_book->id ?? "" }}" class="selected-book-cta-read">
-                 <div class="flex items-center justify-center gap-2">
-                     @if ($selected_book->is_reading ?? false)
-                         Lanjut Baca
-                     @else
-                         Lihat Buku
-                     @endif
-                 </div>
-             </a>
-         </div>
-     </aside>
-     <aside class="aside aside-2"></aside>
-     <aside class="aside aside-3"></aside>
+                <a href="/book/{{ $selected_book->id ?? '' }}" class="selected-book-cta-read">
+                    <div class="flex items-center justify-center gap-2">
+                        @if ($selected_book->is_reading ?? false)
+                            Lanjut Baca
+                        @else
+                            Lihat Buku
+                        @endif
+                    </div>
+                </a>
+            </div>
+        </aside>
+        <aside class="aside aside-2"></aside>
+        <aside class="aside aside-3"></aside>
 
-     @endisset
+    @endisset
 
     <div class="square square-1"></div>
     <div class="square square-2"></div>
@@ -75,91 +75,55 @@
     <br />
     <br />
 
-    @isset($onRead)
-    <section class="main-section">
-        <h1 class="text-2xl">Lanjut Baca Yuk</h1>
+    @isset($on_reads)
+        <section class="main-section">
+            <h1 class="text-2xl">Lanjut Baca Yuk</h1>
 
-        <div class="continue-read-section">
-            @foreach ($onReads as $book)
-                <a class="book" href="/dashboard?selected-book={{ $book->id }}">
-                    <img class="book-image book-image-1 rounded-lg" src="{{ $book->cover_url }}" width="200"
-                        height="280" />
-                    <div class="book-info">
-                        <div>
-                            <p class="book-title font-black">{{ $book->title }}</p>
-                            <p class="book-author">{{ $book->author }}</p>
-                        </div>
-                        <div class="book-action">
-                            <p class="book-progress-label">{{ $book['progress'] }}%</p>
-                            <div class="book-progress">
-                                <div class="mt-4 flex w-full h-full bg-gray-200 rounded-full overflow-hidden"
-                                    role="progressbar" aria-valuenow="{{ $book['progress'] }}" aria-valuemin="0"
-                                    aria-valuemax="100">
-                                    <div class="flex flex-col justify-center rounded-full overflow-hidden bg-orange-200 text-xs text-white text-center whitespace-nowrap transition duration-500"
-                                        style="width: {{ $book['progress'] }}%"></div>
+            <div class="continue-read-section">
+                @foreach ($on_reads as $book)
+                    <a class="book" href="/dashboard?selected-book={{ $book->id }}">
+                        <img class="book-image book-image-1 rounded-lg" src="{{ $book->cover_url }}" width="200"
+                            height="280" />
+                        <div class="book-info">
+                            <div>
+                                <p class="book-title font-black">{{ $book->title }}</p>
+                                <p class="book-author">{{ $book->author }}</p>
+                            </div>
+                            <div class="book-action">
+                                <p class="book-progress-label">{{ $book->progress }}%</p>
+                                <div class="book-progress">
+                                    <div class="mt-4 flex w-full h-full bg-gray-200 rounded-full overflow-hidden"
+                                        role="progressbar" aria-valuenow="{{ $book->progress }}" aria-valuemin="0"
+                                        aria-valuemax="100">
+                                        <div class="flex flex-col justify-center rounded-full overflow-hidden bg-orange-200 text-xs text-white text-center whitespace-nowrap transition duration-500"
+                                            style="width: {{ $book->progress }}%"></div>
+                                    </div>
+                                </div>
+                                <div class="book-cta-read">
+                                    <svg class="icon-play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                        fill="currentColor" class="size-6">
+                                        <path fill-rule="evenodd"
+                                            d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
                                 </div>
                             </div>
-                            <div class="book-cta-read">
-                                <svg class="icon-play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                    fill="currentColor" class="size-6">
-                                    <path fill-rule="evenodd"
-                                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
                         </div>
-                    </div>
-                </a>
-            @endforeach
+                    </a>
+                @endforeach
 
 
-        </div>
-    </section>
+            </div>
+        </section>
     @endisset
 
-    @if(count($trendings) > 0)
-    <section class="main-section">
-        <h1 class="text-2xl my-5">Lagi Trending Minggu Ini</h1>
+    @if (count($trendings) > 0)
+        <section class="main-section">
+            <h1 class="text-2xl my-5">Lagi Trending Minggu Ini</h1>
 
-        <div class="trending-section">
-            @foreach ($trendings as $book)
-                <a class="book" href="/dashboard?selected-book={{ $book->id }}"
-                    id="book">
-                    <img class="book-image book-image-3" src="{{ $book->cover_url }}" width="100"
-                        height="150" />
-                    <div class="book-info">
-                        <div>
-                            <h2 class="book-title font-bold">{{ $book->title }}</h2>
-                            <p class="book-author">{{ $book->author }}</p>
-                        </div>
-                        <div class="book-action">
-                            <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                fill="currentColor" class="size-6">
-                                <path fill-rule="evenodd"
-                                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-                            <span>{{ $book->rating }}</span>
-                        </div>
-                    </div>
-                    </a>
-            @endforeach
-        </div>
-    </section>
-    <br />
-    <br />
-    @endif
-
-
-    @if(count($favorites) > 0)
-    <section class="main-section">
-        <h1 class="text-2xl mb-5">Favorit kamu banget</h1>
-
-        <div class="trending-section">
             <div class="trending-section">
-                @foreach ($favorites as $book)
-                    <a href="/dashboard?selected-book={{ $book->id }}"
-                        class="book">
+                @foreach ($trendings as $book)
+                    <a class="book" href="/dashboard?selected-book={{ $book->id }}" id="book">
                         <img class="book-image book-image-3" src="{{ $book->cover_url }}" width="100"
                             height="150" />
                         <div class="book-info">
@@ -180,12 +144,45 @@
                     </a>
                 @endforeach
             </div>
-        </div>
-    </section>
+        </section>
+        <br />
+        <br />
+    @endif
 
-    <br />
-    <br />
-    <br />
+    @if (count($favorites) > 0)
+        <section class="main-section">
+            <h1 class="text-2xl mb-5">Favorit kamu banget</h1>
+
+            <div class="trending-section">
+                <div class="trending-section">
+                    @foreach ($favorites as $book)
+                        <a href="/dashboard?selected-book={{ $book->id }}" class="book">
+                            <img class="book-image book-image-3" src="{{ $book->cover_url }}" width="100"
+                                height="150" />
+                            <div class="book-info">
+                                <div>
+                                    <h2 class="book-title font-bold">{{ $book->title }}</h2>
+                                    <p class="book-author">{{ $book->author }}</p>
+                                </div>
+                                <div class="book-action">
+                                    <svg class="icon-rating" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                        fill="currentColor" class="size-6">
+                                        <path fill-rule="evenodd"
+                                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    <span>{{ $book->rating }}</span>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+        </section>
+
+        <br />
+        <br />
+        <br />
 
     @endif
 
