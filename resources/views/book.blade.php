ac4ceeaefced6bff23e20c6256c517510f96ba17
Dzaky Fawwaz <dzaky@Dzaky.local>
2024-12-26 21:54:01 +0700

    detail book (ongoing)

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -12,25 +12,56 @@
 </head>
 
 <body>
-    <a class="book" href="/dashboard?selected-book={{ $book->id }}">
-        <img class="book-image book-image-1 rounded-lg" src="{{ $book->cover_url }}" width="200" height="280" />
-        <div class="book-info">
-            <div>
-                <p class="book-title font-black">{{ $book->title }}</p>
-                <p class="book-author">{{ $book->author }}</p>
-            </div>
-            <div class="book-action">
-                <p class="book-progress-label">{{ $book->progress ?? 0 }}%</p>
-                <div class="book-progress">
-                    <div class="mt-4 flex w-full h-full bg-gray-200 rounded-full overflow-hidden" role="progressbar"
-                        aria-valuenow="{{ $book->progress ?? 0 }}" aria-valuemin="0" aria-valuemax="100">
-                        <div class="flex flex-col justify-center rounded-full overflow-hidden bg-orange-200 text-xs text-white text-center whitespace-nowrap transition duration-500"
-                            style="width: {{ $book->progress ?? 0 }}%"></div>
-                    </div>
+    <section class="xl:px-40 lg:px-20 md:px-10 sm:px-10 p-0 pt-14">
+        <div class="xl:flex lg:flex md:flex sm:flex block">
+            <img class="h-96 w-64 rounded-lg" src="{{ $book->cover_url }}" />
+            <div class="p-8">
+                <div>
+                    <h1 class="text-3xl font-bold ">{{ $book->title }}</h1>
+                    <p class="text-white">{{ $book->author }}</p>
                 </div>
+                <div class="flex items-center align-items-center gap-2 mt-2">
+                    <div class="flex gap-1">
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd"
+                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <p class="text-lg mt-1.5">{{ $book->rating ?? '' }}</p>
+                </div>
+
+                <div class="text-white">{{ $book->description }}</div>
+                <div class="mt-8 text-white">{{ $book->synopsis }}</div>
             </div>
         </div>
-    </a>
+
+    </section>
 </body>
 
 </html>
